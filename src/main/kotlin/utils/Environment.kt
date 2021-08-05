2289237dac267ec556dc6248b397ea7170f4aaf8
Daksh <daksh@technowolf.in>
2021-08-05 23:46:23 +0530

    add environment constants for easy access.

---
+++ src/main/kotlin/utils/Environment.kt
@@ -0,0 +1,16 @@
+package utils
+
+object Environment {
+    const val OWNER_ID = "ownerId"
+    const val PREFIX = "prefix"
+    const val TOKEN = "token"
+    const val IS_DEBUG = "isDebug"
+    const val REDDIT_USERNAME = "redditUsername"
+    const val REDDIT_PASSWORD = "redditPassword"
+    const val REDDIT_APP_ID = "redditAppId"
+    const val REDDIT_APP_SECRET = "redditAppSecret"
+    const val REDDIT_USER_AGENT = "redditUserAgent"
+    const val STACKOVERFLOW_API_KEY = "stackoverflowApiKey"
+    const val GENIUS_API_KEY = "geniusApiKey"
+    const val OPEN_WEATHER_API_KEY = "openWeatherKey"
+}
