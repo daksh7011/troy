edaefb167563d5a8db6dcce72a809d7c2fc789e9
Daksh <daksh@technowolf.in>
2021-08-05 23:46:35 +0530

    Add more extensions.

--- src/main/kotlin/utils/Extensions.kt
+++ src/main/kotlin/utils/Extensions.kt
@@ -1,10 +1,16 @@
 package utils
 
+import com.kotlindiscord.kord.extensions.utils.env
 import core.Credits
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.entity.Message
 import io.ktor.http.*
 
 object Extensions {
-    fun Credits.embedUrl(): String {
-        return "[$name](${url.encodeURLPath()}) for ($reason)"
-    }
+    fun Credits.embedUrl(): String = "[$name](${url.encodeURLPath()}) for ($reason)"
+    fun Message.isBot(): Boolean = author?.isBot ?: true
+    fun Message.isNotBot(): Boolean = isBot().not()
+    fun Message.containsF(): Boolean = content.lowercase() == "f"
+    fun Message.containsNigga(): Boolean = content.lowercase().contains("nigga")
+    fun Snowflake.isOwner(): Boolean = asString == env(Environment.OWNER_ID)
 }
