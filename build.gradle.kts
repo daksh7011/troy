d76620779500bfe1f4fdc3da8cb9cf1a0f6ab04b
Daksh <daksh@technowolf.in>
2021-10-16 00:33:27 +0530

    update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,10 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.kord:kord-core:0.7.4")
-    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.4.4-RC2")
-    implementation("io.ktor:ktor-client-core:1.6.2")
-    implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation("dev.kord:kord-core:0.8.0-M7")
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.1-RC1")
+    implementation("io.ktor:ktor-client-core:1.6.4")
+    implementation("org.slf4j:slf4j-simple:1.7.32")
 }
 
 tasks.withType<KotlinCompile> {
